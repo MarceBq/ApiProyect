6883324e982e7ebc647270a228b08bf902c0811f
Anthony Romero <anthony.romero@tranzact.net>
2023-05-11 02:46:48 -0500

    refactor: add services layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import './App.css'
 import { useState, useEffect } from 'react'
-import { State } from 'interfaces'
+
 import SearchBar from 'components/searchBar'
+import { State } from 'interfaces'
+import { getStates } from 'services'
+
+import './App.css'
 
 function App() {
 	const [states, setStates] = useState<State[]>([])
 
 	useEffect(() => {
-		const getStates = async () => {
-			const response = await fetch(
-				'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
-			)
+		const populateStates = async () => {
+			const result = await getStates()
 
-			const jsonStates = await response.json()
-
-			setStates(jsonStates)
+			setStates(result)
 		}
 
-		getStates()
+		populateStates()
 	}, [])
 
 	return (
---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+import { State } from 'interfaces'
+
+export async function getStates(): Promise<State[]> {
+	const response = await fetch(
+		'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+	)
+
+	return await response.json()
+}
